993ca61c414ee065d861ffbebc9ec6e98fafede9
Erdem SÜREN <erdem_suren@windowslive.com>
2024-10-30 15:50:58 +0300

    Add project files.

---
+++ Repositories/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repositories
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> opt) : DbContext(opt)
+    {
+        public DbSet<Product> Products { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
