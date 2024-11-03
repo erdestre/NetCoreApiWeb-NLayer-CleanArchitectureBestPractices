7f4dd3b5f888adf5234482ad11c4be014524186d
Erdem Süren <erdem_suren@windowslive.com>
2024-11-03 16:41:19 +0300

    Route Constraints

--- App.API/Controllers/ProductsController.cs
+++ App.API/Controllers/ProductsController.cs
@@ -10,20 +10,20 @@ namespace App.API.Controllers
 	{
 		[HttpGet]
 		public async Task<IActionResult> GetAll() => CreateActionResult(await productService.GetAllListAsync());
-		[HttpGet("{pageNumber}/{pageSize}")]
+		[HttpGet("{pageNumber:int}/{pageSize:int}")]
 		public async Task<IActionResult> GetPagedAll(int pageNumber, int pageSize) => CreateActionResult(await productService.GetPagedAllListAsync(pageNumber, pageSize));
 		// http://localhost:5000/api/products?id=2
-		[HttpGet("{id}")]
+		[HttpGet("{id:int}")]
 		public async Task<IActionResult> GetById(int id) => CreateActionResult(await productService.GetByIdAsync(id));
 		[HttpPost]
 		public async Task<IActionResult> Create(CreateProductRequest request) => CreateActionResult(await productService.CreateAsync(request));
-		[HttpPut("{id}")]
+		[HttpPut("{id:int}")]
 		public async Task<IActionResult> Update(int id, UpdateProductRequest request) => CreateActionResult(await productService.UpdateAsync(id, request));
 		[HttpPatch("Stock")]
 		public async Task<IActionResult> UpdateStock(UpdateProductStockRequest request) => CreateActionResult(await productService.UpdateStockAsync(request));
 		//[HttpPut("UpdateStock")]
 		//public async Task<IActionResult> UpdateStock(UpdateProductStockRequest request) => CreateActionResult(await productService.UpdateStockAsync(request));
-		[HttpDelete("{id}")]
+		[HttpDelete("{id:int}")]
         public async Task<IActionResult> Delete(int id) => CreateActionResult(await productService.DeleteAsync(id));
     }
 }
