092f2db010369830de3f984955f02350492f6d4c
Erdem Süren <erdem_suren@windowslive.com>
2024-11-17 19:17:17 +0300

    Product Service categoryId added

--- Services/Products/Update/UpdateProductRequestValidator.cs
+++ Services/Products/Update/UpdateProductRequestValidator.cs
@@ -19,8 +19,11 @@ namespace App.Services.Products.Update
             RuleFor(x => x.Price)
                 .GreaterThan(0).WithMessage("Ürün fiyatı 0'dan büyük olmalıdır.");
 
-            //stock inclusiveBetween validation
-            RuleFor(x => x.Stock)
+            RuleFor(x => x.CategoryId)
+	            .GreaterThan(0).WithMessage("Ürün kategori değeri 0'dan büyük olmalıdır.");
+
+			//stock inclusiveBetween validation
+			RuleFor(x => x.Stock)
                 .InclusiveBetween(1, 100).WithMessage("Stok Adedi 1 ile 100 Arasında Olmalıdır.");
         }
     }
